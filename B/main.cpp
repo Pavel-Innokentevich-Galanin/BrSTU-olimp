48fd51f5c0f3ce9f9eb904448d06f628730743c8
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-05-18 00:28:02 +0300

    Galanin_Pavel : Updated task B

--- B/main.cpp
+++ B/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> vec[i];
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        int count = 0;
+        for (int j = 0; j < n; ++j)
+        {
+            if (vec[j] <= 0)
+            {
+                count += 1;
+            }
+            vec[j] -= 1;
+        }
+        if (count == n)
+        {
+            cout << i;
+            return 0;
+        }
+    }
+    cout << n;
+
     return 0;
 }
