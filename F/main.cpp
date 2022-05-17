11b0bde15a218476275586c78bc10a43951c4014
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-05-18 00:34:48 +0300

    Galanin_Pavel : Updated task F

--- F/main.cpp
+++ F/main.cpp
@@ -1,9 +1,66 @@
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
+void isCorrectBreakets(string str)
+{
+    stack<char> myStack;
+    for (int i = 0; i < str.size(); ++i)
+    {
+        if (str[i] == ')')
+        {
+            if (myStack.empty())
+            {
+                cout << "0";
+                return;
+            }
+            myStack.pop();
+        }
+        if (str[i] == '(')
+        {
+            myStack.push('(');
+            // continue;
+        }
+    }
+    cout << "1";
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    string str;
+    // cin >> str;
+    getline(cin, str);
+
+    string breaketsStr = "";
+    for (int i = 0; i < str.size(); ++i)
+    {
+        if (str[i] == '(' || str[i] == ')')
+        {
+            breaketsStr += str[i];
+        }
+        // cout << i << "-"  << str[i] << endl;
+    }
+    //  cout << breaketsStr << endl;
+
+    isCorrectBreakets(breaketsStr);
+    /*
+        stack<char> myStack;
+        for (int i = 0; i < str.size(); ++i) {
+                if (str[i] == ')') {
+                if (myStack.empty()) {
+                        cout << "0";
+                    return 0;
+                }
+                myStack.pop();
+            }
+            if (str[i] == '(') {
+                myStack.push('(');
+                //continue;
+            }
+
+        }
+        cout << "1";*/
+
     return 0;
 }
