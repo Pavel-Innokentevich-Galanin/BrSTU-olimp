7300299a3d844cc022943780558242150ccc3394
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-05-18 00:07:27 +0300

    Galanin_Pavel : Updated task A

--- A/main.cpp
+++ A/main.cpp
@@ -4,6 +4,33 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int w, h, p;
+    cin >> w >> h >> p;
+    if (p % 2 == 1)
+    {
+        cout << "NO";
+        return 0;
+    }
+    p /= 2;
+    p -= w + h;
+    int buf = p - h;
+    for (int i = w - 1; i > 0; i--)
+    {
+        if (buf == i)
+        {
+            cout << "YES";
+            return 0;
+        }
+    }
+    buf = p - w;
+    for (int i = h - 1; i > 0; i--)
+    {
+        if (buf == i)
+        {
+            cout << "YES";
+            return 0;
+        }
+    }
+    cout << "NO";
     return 0;
 }
